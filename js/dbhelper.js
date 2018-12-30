4feda019c9ac3bd76f8a202c5535644da0333524
youngsoo shin <sys920@gmail.com>
2018-12-29 22:13:09 -0600

    adding sw.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,3 +1,13 @@
+if(navigator.serviceWorker) {
+  navigator.serviceWorker.register('sw.js')
+  .then(() => {
+    console.log("Service Worker Installed");
+  })
+  .catch(() => {
+    console.log("Service Worker Did Not Installed");
+  })
+}
+
 /**
  * Common database helper functions.
  */
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 let restaurants,
   neighborhoods,
   cuisines
