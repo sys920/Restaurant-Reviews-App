208843fb2ff68b022eb4b714e87149155009e288
youngsoo shin <sys920@gmail.com>
2019-01-06 16:08:15 -0600

    updating dbhelper img link

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -153,14 +153,14 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return (`restaurant.html?id=${restaurant.id}`);
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`img/${restaurant.photograph}`);
   }
 
   /**
